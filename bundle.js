6bdeff42a8d34de1475d3301aacb38bfeffdfb87
Connor Witt <conwitt@umich.edu>
2018-06-03 18:03:01 -0400

    bundle

--- bundle.js
+++ bundle.js
@@ -1545,7 +1545,7 @@ const textUtil = require('./text_util');
 const audioUtil = require('./audio_util');
 
 // TODO: Generate URLS
-const TRUMP_TWEET_URL = 'http://localhost:8000/trump-markov/';
+const TRUMP_TWEET_URL = 'https://conwitt-markov.herokuapp.com/trump-markov/';
 
 // Vue app
 var potusVsAi = new Vue({
